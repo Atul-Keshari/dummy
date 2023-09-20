5aad8f495616ebf407b613a1f1d88aeeec876606
Atul-Keshari <atulkeshari138@gmail.com>
2023-09-21 02:47:34 +0530

    dummy

---
+++ lib/global/globals.dart
@@ -0,0 +1,7 @@
+import 'package:dummy/auth/google_auth.dart';
+
+var gAuth = GoogleAuth();
+
+// var updateAvailable = false;
+// var versionInfo = {};
+// var noNewNotification = false;
---
+++ lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:dummy/ui/pages/onboarding1.dart';
+import 'package:dummy/ui/pages/undefinedScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:dummy/global/globals.dart' as globals;
+import 'package:dummy/routes/router.dart' as router;
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+late SharedPreferences prefs;
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  // SharedPreferences.getInstance().then((prefs) {
+  //   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+  //       .then((value) => runApp(
+  //             RestartWidget(
+  //               child: MyApp(),
+  //             ),
+  //           ));
+  // });
+  // runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  prefs = await SharedPreferences.getInstance();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(
+            RestartWidget(
+              child: MyApp(),
+            ),
+          ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  void getLoginStatus() async {
+    prefs = await SharedPreferences.getInstance();
+    globals.gAuth.googleSignIn.isSignedIn().then((value) {
+      prefs.setBool("isLoggedin", value);
+    });
+  }
+
+  @override
+  void initState() {
+    getLoginStatus();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      onGenerateRoute: router.generateRoute,
+      onUnknownRoute: (settings) => CupertinoPageRoute(
+          builder: (context) => UndefinedScreen(
+                name: settings.name,
+              )),
+      // theme: Provider.of<ThemeModel>(context).currentTheme,
+      debugShowCheckedModeBanner: false,
+      home: Onboarding(),
+    );
+  }
+}
+
+class RestartWidget extends StatefulWidget {
+  RestartWidget({this.child});
+
+  final Widget? child;
+
+  static void restartApp(BuildContext context) {
+    context.findAncestorStateOfType<_RestartWidgetState>()!.restartApp();
+  }
+
+  @override
+  _RestartWidgetState createState() => _RestartWidgetState();
+}
+
+class _RestartWidgetState extends State<RestartWidget> {
+  Key key = UniqueKey();
+
+  void restartApp() {
+    setState(() {
+      key = UniqueKey();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return KeyedSubtree(
+      key: key,
+      child: widget.child!,
+    );
+  }
+}
